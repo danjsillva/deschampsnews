15bc9fba3c951f005d985043916ff50c31a26d28
Daniel Silva <danjsillva@gmail.com>
2026-05-19 12:09:35 -0300

    chore: upgrade project dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,13 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   {
     plugins: {
-      import: (await import("eslint-plugin-import")).default,
+      import: importPlugin,
     },
     rules: {
       "import/order": [
